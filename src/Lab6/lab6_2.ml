a29a140e8e6923639b8891fa7774e86e0bd5ec36
MfalconP <maryushp@gmail.com>
2022-12-04 13:08:56 +0100

    lab 6 added

--- src/Lab6/lab6_2.ml
+++ src/Lab6/lab6_2.ml
@@ -1,1 +1,26 @@
-let pascalCascade
+let pascalCascadeI n =
+    if (n<1) then failwith("There is no such line in pascal cascade")
+    else
+        let i = ref 1 and arr1 = ref (Array.make 1 1) and arr2 = ref (Array.make 2 1);
+        in begin
+        	    while (!i < n) do
+        	    arr2 := Array.make (!i + 1) 1;
+
+        	    for l=1 to (!i - 1) do
+                    !arr2.(l) <- (!arr2.(l-1) + !arr1.(l))
+                    done;
+
+                !arr2.(!i) <- !arr2.(!i-1);
+        	    i := !i + 1;
+
+                arr1 := Array.make (!i + 1) 1;
+                arr1 := Array.copy !arr2
+        	    done;
+            !arr1
+            end
+;;
+
+
+
+pascalCascadeI 6;;
+
